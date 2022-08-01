983eaeec87bd98ee4e84a153fe5af10d8525b662
rhyanfauzan <rhyananiki@gmail.com>
2022-08-01 12:28:56 +0700

    login UI (finishing)

--- lib/presentation/page/auth/login_page.dart
+++ lib/presentation/page/auth/login_page.dart
@@ -15,72 +15,114 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final controllerEmail = TextEditingController();
   final controllerPassword = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  login() {
+    if (formKey.currentState!.validate()) {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: LayoutBuilder(
       builder: (context, constraints) {
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            DView.nothing(),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                children: [
-                  Image.asset(
-                    AppAsset.logo,
-                    scale: 20,
-                  ),
-                  DView.spaceHeight(40),
-                  TextField(
-                    controller: controllerEmail,
-                    decoration: InputDecoration(
-                        fillColor: AppColor.primary.withOpacity(0.5),
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide.none,
-                        ),
-                        hintText: 'email',
-                        isDense: true,
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 16,
-                        )),
-                  ),
-                  TextField(
-                    controller: controllerPassword,
-                    decoration: InputDecoration(
-                        fillColor: AppColor.primary.withOpacity(0.5),
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide.none,
-                        ),
-                        hintText: 'password',
-                        isDense: true,
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 16,
-                        )),
-                  ),
-                ],
-              ),
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight,
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 16, 16, 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Belum punya akun? '),
-                  Text(
-                    'Register',
-                    style: TextStyle(color: AppColor.primary),
-                  )
-                ],
-              ),
-            )
-          ],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                DView.nothing(),
+                Form(
+                  key: formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          AppAsset.logo,
+                          width: 70,
+                        ),
+                        DView.spaceHeight(40),
+                        TextFormField(
+                          controller: controllerEmail,
+                          validator: (value) =>
+                              value == '' ? 'email harus diisi.' : null,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          decoration: InputDecoration(
+                              fillColor: AppColor.primary.withOpacity(0.5),
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none,
+                              ),
+                              hintText: 'email',
+                              isDense: true,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 16,
+                              )),
+                        ),
+                        DView.spaceHeight(),
+                        TextFormField(
+                          controller: controllerPassword,
+                          validator: (value) =>
+                              value == '' ? 'password harus diisi.' : null,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              fillColor: AppColor.primary.withOpacity(0.5),
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none,
+                              ),
+                              hintText: 'password',
+                              isDense: true,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 16,
+                              )),
+                        ),
+                        DView.spaceHeight(30),
+                        Material(
+                          color: AppColor.primary,
+                          borderRadius: BorderRadius.circular(30),
+                          child: InkWell(
+                            onTap: () => login(),
+                            borderRadius: BorderRadius.circular(30),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 40, vertical: 16),
+                              child: Text(
+                                'LOGIN',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 16, 16, 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Belum punya akun? '),
+                      Text(
+                        'Register',
+                        style: TextStyle(color: AppColor.primary),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         );
       },
     ));
