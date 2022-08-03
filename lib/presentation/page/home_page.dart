4a70df770f733011446b037da708efc4b3180fa5
rhyanfauzan <rhyananiki@gmail.com>
2022-08-03 09:08:58 +0700

    Home page part1

--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -1,29 +1,75 @@
-import 'package:course_money_record/config/session.dart';
-import 'package:course_money_record/presentation/page/auth/login_page.dart';
+import 'package:course_money_record/config/app_asset.dart';
+import 'package:course_money_record/config/app_color.dart';
+import 'package:course_money_record/presentation/controller/c_user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final cUser = Get.put(CUser());
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Row(
-        children: [
-          Text('Home Page'),
-          IconButton(
-            onPressed: () {
-              Session.clearUser();
-              Get.off(() => const LoginPage());
-            },
-            icon: Icon(Icons.logout),
+      endDrawer: Drawer(),
+      body: SingleChildScrollView(
+        child: Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 50, 20, 30),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      AppAsset.profile,
+                      width: 22,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Hi, '),
+                          Obx(() {
+                            return Text(cUser.data.name ?? '');
+                          })
+                        ],
+                      ),
+                    ),
+                    Builder(builder: (ctx) {
+                      return Material(
+                        borderRadius: BorderRadius.circular(4),
+                        color: AppColor.primary,
+                        child: InkWell(
+                          onTap: () {
+                            Scaffold.of(ctx).openEndDrawer();
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Icon(
+                              Icons.menu,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      );
+                    })
+                  ],
+                ),
+              )
+            ],
           ),
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
